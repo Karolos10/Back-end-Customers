e4705b505b334abbbc99a559d962c984ac079787
Karlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2024-02-17 17:16:06 -0500

    Backend apirest for customers

---
+++ src/main/java/com/icodeap/crudfullstackangular/service/CustomerServiceImpl.java
@@ -0,0 +1,51 @@
+package com.icodeap.crudfullstackangular.service;
+
+import com.icodeap.crudfullstackangular.entity.Customer;
+import com.icodeap.crudfullstackangular.exception.ResourceNotFoundException;
+import com.icodeap.crudfullstackangular.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer findById(Integer id) {
+        Customer customer = customerRepository.findById(id).orElseThrow(
+                () ->{
+                    throw new ResourceNotFoundException("Customer con id" +id+ "No se encuentra");
+                }
+        );
+
+        return customer;
+        //return customerRepository.findById(id).get();
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        customerRepository.deleteById(id);
+
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+        return customerRepository.save(customer);
+    }
+}
